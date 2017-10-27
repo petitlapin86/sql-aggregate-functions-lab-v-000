5083afa99e3f1106e6db0a5fa89bdf9de7a0a808
Paige Skinner <skinnerpa86@yahoo.com>
2017-10-27 14:14:28 +0000

    automatically backed up by learn

--- lib/students.rb
+++ lib/students.rb
@@ -7,7 +7,7 @@ def lowest_student_gpa
 end
 
 def average_student_gpa
-  "SELECT AVG #average_student_gpa FROM insert.sql;"
+  "SELECT AVG #average_student_gpa FROM insert;"
 end
 
 def total_tardies_for_all_students
