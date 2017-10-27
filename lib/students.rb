06f8f63ec8ec66a0d138faf2c00e64aa505e9564
Paige Skinner <skinnerpa86@yahoo.com>
2017-10-27 14:27:34 +0000

    automatically backed up by learn

--- lib/students.rb
+++ lib/students.rb
@@ -13,7 +13,7 @@ def average_student_gpa
 end
 
 def total_tardies_for_all_students
-"SELECT COUNT (*) FROM students WHERE tardies;"
+"SELECT COUNT (*) FROM tardies;"
 end
 
 def average_gpa_for_9th_grade
